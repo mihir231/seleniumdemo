3ac3ddec01c404e28a2d36bbc85081b0dd26853e
mihir231 <87566558+mihir231@users.noreply.github.com>
2021-09-17 17:03:36 +0530

    Add files via upload

---
+++ src/test/java/selenium1/test1/googledemo.java
@@ -0,0 +1,36 @@
+package selenium1.test1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+	public class googledemo {
+		private WebDriver driver;
+
+		@Test(priority=0) 
+		public void testJbkTitle() {
+		    driver.get("http://www.google.com"); 
+		    String title = driver.getTitle(); 
+		    System.out.println(title);
+		    Assert.assertEquals(title,"Google");
+		   
+		}
+
+			@BeforeTest
+			public void beforeTest() {
+				System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			}
+
+			@AfterTest
+			public void afterTest() {
+				driver.quit();
+			}
+		}
+
+
+
